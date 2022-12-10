7d25dcb5c99abbbca0f61b22c677b4a515883f60
Lynnmich <lynnenmichuki@gmail.com>
2022-12-10 00:52:25 -0800

    Lists all records in decending order

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- script 10
+-- script that lists all the records of the table second_table of the database hbtn_0c_0 in your MySQL server
+SELECT score, name FROM second_table ORDER BY score DESC;
