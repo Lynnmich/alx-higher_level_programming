fddc96892c18f753b7dbff744b8dd7e2c9fabfac
Lynnmich <lynnenmichuki@gmail.com>
2023-04-12 03:06:13 +0000

    8th commit

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 const argv = process.argv;
 const i = parseInt(argv[2]);
 if (isNaN(i)) {
-console.log('Missing number of occurrrences');
+  console.log('Missing number of occurrrences');
 } else {
   for (let j = 0; j < i; j++) {
     console.log('C is fun');
