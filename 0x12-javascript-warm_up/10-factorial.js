91b01c9e372ccb268af40ba7a0df32ff32db4d65
Lynnmich <lynnenmichuki@gmail.com>
2023-04-12 04:40:08 +0000

    11th commit

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const argv = process.argv;
+const i = parseInt(process.argv[2]);
+function factor (num) {
+  if (num > 1) {
+  return num * factor(num - 1);
+} else {
+  return 1;
+  }
+}
+console.log(factor(i));
