58673779af33b65155203b800e551f98fad82e3c
Lynnmich <lynnenmichuki@gmail.com>
2023-04-13 04:00:59 +0000

    JS

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 console.log(list);
-const newList = list.map((num, element)=> num * element);
+const newList = list.map((num, element) => num * element);
 console.log(newList);
