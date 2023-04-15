32334e63dd4eef69343581fd7131198dd87b36d6
Lynnmich <lynnenmichuki@gmail.com>
2023-04-15 03:50:01 -0700

    Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -14,7 +14,13 @@ if __name__ == '__main__':
             host='localhost',
             port=3306,)
     cursor = db.cursor()
+
     cursor.execute('SELECT * FROM states ORDER BY states.id ASC;')
+
     states = cursor.fetchall()
+
     for state in states:
         print(state)
+
+    cursor.close()
+    db.close()
