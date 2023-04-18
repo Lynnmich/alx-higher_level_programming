7f02ef051ce55d03edc588fc2e08f8dd94b10344
Lynnmich <lynnenmichuki@gmail.com>
2023-04-17 23:06:59 -0700

    Tasks

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+City class that inherits from the Base class
+"""
+from sqlalchemy import ForeignKey, Column, String, Integer
+from sqlalchemy.ext.declrarative import declarative_base
+
+Base = declarative_base
+
+
+class City(Base):
+    """This class inherits attributes from the Base class"""
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
