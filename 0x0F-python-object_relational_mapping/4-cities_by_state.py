1a0d26db8d386b545a8586ca20b3593715df2419
Lynnmich <lynnenmichuki@gmail.com>
2023-04-19 02:54:02 -0700

    tasks

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,7 +5,7 @@ A script that lists all from the database hbtn_0e_4_usa
 import sys
 import MySQLdb
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     db = MySQLdb.connect(
             user=sys.argv[1], passwd=sys.argv[2],
             db=sys.argv[3],
