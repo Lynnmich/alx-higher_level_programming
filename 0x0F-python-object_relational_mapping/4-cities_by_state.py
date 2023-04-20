db337d02e68481cb605f0a7e6a88f1841dea5db5
Lynnmich <lynnenmichuki@gmail.com>
2023-04-19 20:40:44 -0700

    Tasks

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -13,8 +13,8 @@ if __name__ == '__main__':
             port=3306)
     cursor = db.cursor()
 
-    cursor.execute("SELECT cities.id, cities.name, state.name FROM cities\
-                   JOIN states ON cities.state_id = state.id\
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                   JOIN states ON cities.state_id = states.id\
                    ORDER BY cities.id ASC")
 
     cities = cursor.fetchall()
