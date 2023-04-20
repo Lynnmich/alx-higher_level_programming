f272bdb4fa80c89fafa9dce3867294d157c93451
Lynnmich <lynnenmichuki@gmail.com>
2023-04-19 20:47:06 -0700

    Tasks

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,9 +14,9 @@ if __name__ == '__main__':
             port=3306)
     cursor = db.cursor()
 
-    cursor.execute("SELECT cities.id, cities.name, state.name FROM cities \
-                   JOIN states ON cities.state_id = states.id \
-                   WHERE states.name = '%s' \
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                   INNER JOIN states ON cities.state_id = states.id\
+                   WHERE states.name = '%s'\
                    ORDER BY cities.id ASC", (sys.argv[4],))
 
     cities = cursor.fetchall()
