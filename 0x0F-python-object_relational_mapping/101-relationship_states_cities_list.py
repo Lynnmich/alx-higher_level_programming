701662d93ba1aba6775da89cf451ee8ef9984674
Lynnmich <lynnenmichuki@gmail.com>
2023-04-21 10:49:46 -0700

    Tasks

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
 
     # print output
     for state in states:
-        print('{}:'.format(state_name))
+        print('{}: {}'.format(state.id, state.name))
         for city in state.cities:
             print('\t{}: {}'.format(city.id, city.name))
 
