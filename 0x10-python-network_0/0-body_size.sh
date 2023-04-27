db8e2df12eb5124fe70f9167d79c2acb028b53a2
Lynnmich <lynnenmichuki@gmail.com>
2023-04-27 12:47:47 -0700

    Task

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script that displays the size of the body of the response
-curl -sI "$1" | grep  Content-Length | cut -d ' ' -f2
+curl -sI "$1" | grep "Content-Length" | cut -d ' ' -f 2
