0f4386172901ea41384cda707f8e566cbad86a0c
lynnmich <lynnenmichuki@gmail.com>
2023-04-28 04:41:37 -0700

    Tasks

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     with urllib.response.urlopen(url) as response:
         html = response.read()
 
-    print("Body response:")
-    print("/t- type: {}".format(type(html)))
-    print("/t- content: {}".format(html))
-    print("/t- utf8 content: {}".format(html.decode('utf-8')))
+        print("Body response:")
+        print("/t- type: {}".format(type(html)))
+        print("/t- content: {}".format(html))
+        print("/t- utf8 content: {}".format(html.decode('utf-8')))
