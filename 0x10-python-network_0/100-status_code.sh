f22baea8d9097d2ca6a8d8f9319b48f2e071a38f
lynnmich <lynnenmichuki@gmail.com>
2023-04-30 23:25:08 -0700

    Advanced task

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,4 @@
-#!/bin/env bash
+#!/bin/bash
 # Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response
 
 curl_response=$(curl -I -s "$1")
