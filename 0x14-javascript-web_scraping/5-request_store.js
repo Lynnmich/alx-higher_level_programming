2fbd6bedd43d27b451174a41170c6b92d5763bb5
Lynne Michuki <lynnenmichuki@gmail.com>
2023-05-23 22:42:16 +0300

    Task

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,19 +4,8 @@
 const request = require('request');
 const fs = require('fs');
 
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    fs.writeFile(filePath, body, 'utf-8', (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(`Webpage content saved to ${filePath}`);
-      }
-    });
+request(process.argv[2], function (err, response, body) {
+  if (err == null) {
+    fs.writeFileSync(process.argv[3], body);
   }
 });
