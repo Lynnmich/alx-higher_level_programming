5e227309608cde2949dd9b6f49b1034e912e9d63
Lynne Michuki <lynnenmichuki@gmail.com>
2023-05-23 22:38:47 +0300

    Task

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,26 +3,26 @@
 
 const request = require('request');
 
-const apiUrl = process.argv[2];
+const url = process.argv[2];
 
-request(apiUrl, (error, response, body) => {
+request(url, (error, response, body) => {
   if (error) {
     console.log(error);
-  } else {
+  } else if (response.statusCode === 200) {
+    const completed = {};
     const tasks = JSON.parse(body);
-
-    const completedTasksByUser = {};
-
-    tasks.forEach((task) => {
-      if (task.completed) {
-        if (completedTasksByUser[task.userId]) {
-          completedTasksByUser[task.userId]++;
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
         } else {
-          completedTasksByUser[task.userId] = 1;
+          completed[task.userId]++;
         }
       }
-    });
-
-    console.log(completedTasksByUser);
+    }
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
 });
