9134194eec49085735f94fefe6f69d081e938fec
Lynne Michuki <lynnenmichuki@gmail.com>
2023-05-23 22:08:30 +0300

    Task

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// A script  that prints the number of movies where the character “Wedge Antilles” is present
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+const characterId = 18;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const films = JSON.parse(body).results;
+    const count = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)).length;
+    console.log(count);
+  }
+});
